b7adb50c12b0c76ce29ecbf4c1734d4533307db4
Ben Randall <brandall@composablesystems.net>
2017-07-28 16:07:37 -0500

    Added a roadmap plan to guide development a bit

--- Main/Server/Client.py
+++ Main/Server/Client.py
@@ -14,7 +14,6 @@ class ClientConnection:
         
     def loop(self):
         print "Player connected with instance ID : " + str(self.ID)
-        Thread(target=self.fakeDamage).start()
         while self.client:
             try:
                 packet = self.client.recv(self.dataSize)
@@ -28,15 +27,3 @@ class ClientConnection:
         packet = Serialization.pack(message, data)
         if self.client:
             self.client.send(packet)
-        
-    def fakeDamage(self):
-        health = 200
-        mana = 100
-        
-        while health > 0 or mana > 0:
-            if(health > 0):
-                health -= 1
-            if(mana > 0):
-                mana -= 1
-            self.send(PacketTypes.CHARACTER_VITAL_TICK, {"curHealth": health, "maxHealth": 200, "curMana": mana, "maxMana": 100})
-            sleep(0.25)
--- README.txt
+++ README.txt
@@ -31,3 +31,29 @@ RUN CLIENT
 2) Go into "Client"
 3) run "run.bat"
 4) You will now connect to your local server
+
+###########################################
+#
+#	FUTURE ROAD MAP
+#
+###########################################
+
+0.1
+- Character Creation
+- 4+ races
+- 4+ classes
+- UI implementation
+- Database hooks save/load
+
+0.2
+- Exploration
+- Make short dungeon
+- Rooms with descriptions and exits
+- Client UI to represent available directions
+- 'See' other players in room
+- 'Chat' with other players in room
+
+0.3
+- Social
+- Friends List - See online/offline friends
+- Direct Messages - Individual windows to allow for player <-> player chat
