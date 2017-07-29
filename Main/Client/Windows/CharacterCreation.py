932dadea8f1c770e48bcbef88a955daa300f8294
Ben Randall <brandall@composablesystems.net>
2017-07-29 16:24:56 -0400

    Have race/class change callbacks hooked up. Just need to create races/classes. As well as 6 point allocation up to 20 max

--- Main/Client/Windows/CharacterCreation.py
+++ Main/Client/Windows/CharacterCreation.py
@@ -17,12 +17,20 @@ class CharacterCreationWindow(Frame):
         self.classes = { "Warrior", "Rogue", "Magician" }
         self.raceVar = StringVar()
         self.raceVar.set("Human")
+        self.raceVar.trace("w", self.raceChanged)
         self.classVar = StringVar()
         self.classVar.set("Warrior")
+        self.classVar.trace("w", self.classChanged)
+    
+    def raceChanged(self, *args):
+        print "Current Race: {}".format(self.raceVar.get())
+    
+    def classChanged(self, *args):
+        print "Current Class: {}".format(self.classVar.get())
     
     def makeWindow(self):
-        self.master.minsize(500, 500)
-        self.master.maxsize(500, 500)
+        self.master.minsize(500, 400)
+        self.master.maxsize(500, 400)
         
         x = 9
         y = 6
@@ -36,42 +44,42 @@ class CharacterCreationWindow(Frame):
         
         # Race
         Label(self.master, text="Race : ", font=("Helvetica", 14)).grid(row=1, column=1, sticky=E)
-        OptionMenu(self.master, self.raceVar, *self.races).grid(row=1, column=2, columnspan=2, sticky=W)
+        OptionMenu(self.master, self.raceVar, *self.races).grid(row=1, column=2, columnspan=2, sticky=W+E)
         
         # Class
         Label(self.master, text="Class : ", font=("Helvetica", 14)).grid(row=1, column=5, sticky=E)
-        OptionMenu(self.master, self.classVar, *self.classes).grid(row=1, column=6, columnspan=2, sticky=W)
+        OptionMenu(self.master, self.classVar, *self.classes).grid(row=1, column=6, columnspan=2, sticky=W+E)
         
         # Stats
         strength = Label(self.master, text="STR : ", font=("Helvetica",14))
         strength.grid(row=2, column=2, sticky=E)
         #strength.bind('<ENTER>', self.hoverStrength)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=2, column=3, sticky=W)
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=2, column=3, sticky=W)
         
         constitution = Label(self.master, text="CON : ", font=("Helvetica",14))
         constitution.grid(row=2, column=5, sticky=E)
         #constitution.bind("ENTER", self.hoverConstitution)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=2, column=6, sticky=W)
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=2, column=6, sticky=W)
     
         dexterity = Label(self.master, text="DEX : ", font=("Helvetica",14))
         dexterity.grid(row=3, column=2, sticky=E)
         #dexterity.bind("ENTER", self.hoverDexterity)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=3, column=3, sticky=W)
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=3, column=3, sticky=W)
     
         agility = Label(self.master, text="AGI : ", font=("Helvetica",14))
         agility.grid(row=3, column=5, sticky=E)
         #agility.bind("ENTER", self.hoverAgility)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=3, column=6, sticky=W)
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=3, column=6, sticky=W)
     
         intelligence = Label(self.master, text="INT : ", font=("Helvetica",14))
         intelligence.grid(row=4, column=2, sticky=E)
         #intelligence.bind("ENTER", self.hoverIntelligence)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=4, column=3, sticky=W)
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=4, column=3, sticky=W)
     
         wisdom = Label(self.master, text="WIS : ", font=("Helvetica",14))
         wisdom.grid(row=4, column=5, sticky=E)
         #wisdom.bind("ENTER", self.hoverWisdom)
-        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12)).grid(row=4, column=6, sticky=W)        
+        Label(self.master, text="10", relief=SUNKEN, font=("Helvetica", 12), padx=5).grid(row=4, column=6, sticky=W)        
         
         
         
